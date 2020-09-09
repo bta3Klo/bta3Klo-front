e9e8ffc5b7abdf9e0b4715790ad871e01965922f
Mayis-Mazin <you@example.com>
2020-09-09 19:01:19 +0300

    adding Screens(home/profile/signin/signup) Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import NavBar from './components/NavBar'
+import './App.css'
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './components/screens/Home'
+import SignIn from './components/screens/SignIn'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>  
+
+    <Route exact path='/'>
+    <Home/> 
+   </Route>
+
+   <Route path='/SignIn'>
+    <SignIn/> 
+   </Route>
+
+   <Route path='/Profile'>
+   <Profile/>
+   </Route>
+
+   <Route path='/Signup'>
+    <Signup /> 
+   </Route>
+
+    </BrowserRouter>
   );
 }
 
