52962c652926bddfc4112d6dc009466104a6e15a
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-09-13 10:51:37 +0300

    sginUp ready need validation edit navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from  'materialize-css/dist/js/materialize.min.js';
+import navLogo from './imges/bta3-klo-nav.png'
+
 const NavBar=()=>{
-    return ( <nav>
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo left">Bta3Klo</Link>
-          <ul id="nav-mobile" className="right">
-            <li><Link to="/SignIn">SignIn</Link></li>
-            <li><Link to="/Signup">Signup</Link></li>
-            <li><Link to="Profile">My profile</Link></li>
-          </ul>
-        </div>
-      </nav>)
+
+  useEffect(() => {
+    let sidenav = document.querySelector('#mobile-menu');
+    M.Sidenav.init(sidenav, {});
+  })
+
+    return (
+    <nav class="nav-wrapper teal accent-4" id="nav">
+    <div class="container">
+      <Link to="/" class="brand-logo"><img src={navLogo} style={{top: '-55px', position: "absolute"}} /></Link>
+      <a href="" class="sidenav-trigger" data-target='mobile-menu'>
+        <i class="material-icons">menu</i>
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><Link to="/Signup">Creat account</Link></li>
+        <li><Link to="/SignIn">SignIn</Link></li>
+        <li><Link to="">Contact Us</Link></li>
+        <li><Link to="">About Us</Link></li>
+      </ul>
+      <ul class="sidenav teal accent-4" id="mobile-menu">
+        <li><Link to="/Signup">Creat account</Link></li>
+        <li><Link to="/SignIn">SignIn</Link></li>
+        <li><Link to="">Contact Us</Link></li>
+        <li><Link to="">About U</Link></li>
+      </ul>
+    </div>
+  </nav>
+  )
 }
 export default NavBar
